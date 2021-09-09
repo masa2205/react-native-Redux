cc8c54a138ee41799982d866d9fa0804c375e006
Masa <katsukatsu.masa.0522@gmail.com>
2021-09-09 17:11:51 +0900

    store edit

--- src/screens/ResultScrenn.js
+++ src/screens/ResultScrenn.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-
-export default App = () => {
-  return(
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  )
+"use strict";
+function main(lines) { // lines: Array<string>
+  /**
+   * このコードは標準入力と標準出力を用いたサンプルコードです。
+   * このコードは好きなように編集・削除してもらって構いません。
+   *
+   * This is a sample code to use stdin and stdout.
+   * You can edit and even remove this code as you like.
+  */
+  lines.forEach((v, i) => console.log(`lines[${i}]: ${v}`));
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
+function runWithStdin() {
+  let input = "";
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+
+  process.stdin.on("data", v => {
+    input += v;
+  });
+  process.stdin.on("end", () => {
+    main(input.split("\n"));
+  });
+}
+runWithStdin();
--- store/actions/user.js
+++ store/actions/user.js
@@ -1,11 +1,18 @@
+// Stateの数字を+1させる
+// Actionの枠組みはほとんど決まっている
+
+// stateの数字を+1させるtype
 export const plusOne = () => {
   return {
-    type: 'PlUS_ONE',
+    type: 'PlUS_ONE', //Actionの種類を識別
+    //payload
   };
 };
 
+// Stateの数字を-1させるtype
 export const minusOne = () => {
   return {
     type: 'MINUS_ONE',
+    //payload
   };
 };
