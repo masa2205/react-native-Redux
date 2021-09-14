229fdc0fa32c4d27e300e8da989ccec3948f6084
Masa <katsukatsu.masa.0522@gmail.com>
2021-09-14 21:55:49 +0900

    first commit

--- store/index.js
+++ store/index.js
@@ -1,1 +1,10 @@
-import {combineReducers} from 'redux' // 各reducerを呼び出して初期状態を取り出す。初期状態をまとめて初期状態ツリーを作る。reducerの処理をまとめたcombination関数を返す。
+import {createStore, combineReducers} from 'redux' // 各reducerを呼び出して初期状態を取り出す。初期状態をまとめて初期状態ツリーを作る。reducerの処理をまとめたcombination関数を返す。
+import userReducer from './reducers/user';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
